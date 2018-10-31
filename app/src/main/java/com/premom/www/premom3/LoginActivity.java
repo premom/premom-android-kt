62ea7c8ba69a14167c6edfdb1e340389670727e6
오혜지 <ohyeji@ohyejiui-MacBook-Pro-415.local>
2018-10-31 21:03:01 +0900

    몇줄 삭제

--- app/src/main/java/com/premom/www/premom3/LoginActivity.java
+++ app/src/main/java/com/premom/www/premom3/LoginActivity.java
@@ -20,9 +20,9 @@ public class LoginActivity extends AppCompatActivity {
     public void fl_BtnClick(View v){
         Intent ref_Intent;
 
-        ref_Intent = new Intent(packageContext: this, TestNextIntentActivity.class);
+//        ref_Intent = new Intent(packageContext this, TestNextIntentActivity.class);
 
-        startActivity(ref_Intent);
+//        startActivity(ref_Intent);
 
     }
 }
