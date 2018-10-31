5bb6a47d4db1b40ec8c2e73b414d6dc290a1ee13
오혜지 <ohyeji@ohyejiui-MacBook-Pro-415.local>
2018-10-31 19:38:22 +0900

    test

--- app/src/main/java/com/premom/www/premom3/MainActivity.java
+++ app/src/main/java/com/premom/www/premom3/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
