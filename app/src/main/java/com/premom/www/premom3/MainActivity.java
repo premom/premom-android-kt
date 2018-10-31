fab837373baa9aa0816a43b83b5d4334bdb01f42
오혜지 <ohyeji@ohyejiui-MacBook-Pro-415.local>
2018-10-31 20:55:31 +0900

    test

--- app/src/main/java/com/premom/www/premom3/MainActivity.java
+++ app/src/main/java/com/premom/www/premom3/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // test
+        // hyez test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
