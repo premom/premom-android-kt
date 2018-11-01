9ce1eeba0ff899f145c1fe291a81e600fc8b9f11
sinda72 <sinda72@gmail.com>
2018-11-01 20:55:57 +0900

    login

--- app/src/main/java/com/premom/www/premom3/DiaryActivity.java
+++ app/src/main/java/com/premom/www/premom3/DiaryActivity.java
@@ -5,26 +5,31 @@ import android.os.Bundle;
 import android.widget.TabHost;
 
 public class DiaryActivity extends AppCompatActivity {
-/*
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_diary);
+       setContentView(R.layout.activity_diary);
 
-        TabHost tabHost = getTabHost();
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
+        tabHost1.setup() ;
 
-        tabHost.addTab(tabHost.newTabSpec("tag")
-                .setIndicator("첫 번째 탭")
-                .setContent(R.id.tab1));
+        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
+        ts1.setContent(R.id.tab1) ;
+        ts1.setIndicator("TAB 1") ;
+        tabHost1.addTab(ts1)  ;
 
-        tabHost.addTab(tabHost.newTabSpec("tag")
-                .setIndicator("두 번째 탭")
-                .setContent(R.id.tab2));
+        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
+        ts2.setContent(R.id.tab2) ;
+        ts2.setIndicator("TAB 2") ;
+        tabHost1.addTab(ts2) ;
 
-        tabHost.addTab(tabHost.newTabSpec("tag")
-                .setIndicator("세 번째 탭")
-                .setContent(R.id.tab3));
-
-        tabHost.setCurrentTab(0);
-    }*/
+        // 세 번째 Tab. (탭 표시 텍스트:"TAB 3"), (페이지 뷰:"content3")
+        TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3") ;
+        ts3.setContent(R.id.tab3) ;
+        ts3.setIndicator("TAB 3") ;
+        tabHost1.addTab(ts3) ;
+    }
 }
