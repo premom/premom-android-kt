53b9f46cccbb4bfda2746a5e404167c8cff7a8c9
sinda72 <sinda72@gmail.com>
2018-11-01 22:43:21 +0900

    login

--- app/src/main/java/com/premom/www/premom3/DiaryActivity.java
+++ app/src/main/java/com/premom/www/premom3/DiaryActivity.java
@@ -3,21 +3,31 @@ package com.premom.www.premom3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TabHost;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import android.view.View;
+import android.widget.TextView;
 
 public class DiaryActivity extends AppCompatActivity {
+    Date mDate;
+    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd");
+    TextView mTextView;
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_diary);
 
+
+       mTextView = (TextView) findViewById(R.id.TextView);
+        mTextView.setText(getTime());
         TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
         tabHost1.setup() ;
 
         // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
         TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
         ts1.setContent(R.id.tab1) ;
-        ts1.setIndicator("TAB 1") ;
+        ts1.setIndicator("Calendar") ;
         tabHost1.addTab(ts1)  ;
 
         // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
@@ -29,7 +39,14 @@ public class DiaryActivity extends AppCompatActivity {
         // 세 번째 Tab. (탭 표시 텍스트:"TAB 3"), (페이지 뷰:"content3")
         TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3") ;
         ts3.setContent(R.id.tab3) ;
-        ts3.setIndicator("TAB 3") ;
+        ts3.setIndicator("Diary") ;
         tabHost1.addTab(ts3) ;
+
+
+    }
+    private String getTime(){
+        mDate = new Date();
+        return mFormat.format(mDate);
+
     }
 }
