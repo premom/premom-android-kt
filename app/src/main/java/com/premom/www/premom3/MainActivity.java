300b7006d4ea3e0658307e9cc6ae244c7be569bb
오혜지 <ohyeji@ohyejiui-MacBook-Pro-415.local>
2018-11-01 20:16:17 +0900

    menu complete

--- app/src/main/java/com/premom/www/premom3/MainActivity.java
+++ app/src/main/java/com/premom/www/premom3/MainActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    Button m_refButton; //뒤로가기
+    Button go_seat_txt;
+    ImageButton go_seat_img;
+    Button go_diary_txt;
+    ImageButton go_diary_img;
+    Button go_setting_txt;
+    ImageButton go_setting_img;
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -16,18 +22,61 @@ public class MainActivity extends AppCompatActivity {
 
         this.setContentView(R.layout.activity_main);
 
+        setViewId();
+
+        go_seat_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goSeatActivity();
+            }
+        });
+
+        go_seat_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goSeatActivity();
+            }
+        });
+
+        go_diary_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goDiaryActivity();
+            }
+        });
+
+        go_diary_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goDiaryActivity();
+            }
+        });
+
+        go_setting_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goSettingActivity();
+            }
+        });
+
+        go_setting_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goSettingActivity();
+            }
+        });
     }
 
-    public void sl_BtnClick(View v){
-        Intent ref_Intent;
-        ref_Intent = new Intent(this, LoginActivity.class);
-        startActivity(ref_Intent);
-
-        finish();
-
+    public void setViewId() {
+        go_seat_txt = (Button) findViewById(R.id.menu_seat_txt);
+        go_seat_img = (ImageButton) findViewById(R.id.menu_seat_img);
+        go_diary_txt = (Button) findViewById(R.id.menu_diary_txt);
+        go_diary_img = (ImageButton) findViewById(R.id.menu_diary_img);
+        go_setting_txt = (Button) findViewById(R.id.menu_setting_txt);
+        go_setting_img = (ImageButton) findViewById(R.id.menu_setting_img);
     }
 
-    public void m1_BtnClick(View v){
+    public void goSeatActivity(){
         Intent m1_Intent;
         m1_Intent = new Intent(this, SeatActivity.class);
         startActivity(m1_Intent);
@@ -36,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void m2_BtnClick(View v){
+    public void goDiaryActivity(){
         Intent m2_Intent;
         m2_Intent = new Intent(this, SettingActivity.class);
         startActivity(m2_Intent);
@@ -45,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void m3_BtnClick(View v){
+    public void goSettingActivity(){
         Intent m3_Intent;
         m3_Intent = new Intent(this, SettingActivity.class);
         startActivity(m3_Intent);
@@ -53,7 +102,5 @@ public class MainActivity extends AppCompatActivity {
         finish();
 
     }
-
-
 
 }
