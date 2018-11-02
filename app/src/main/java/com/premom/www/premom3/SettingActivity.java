b355955597a9bea62e5dc4e503c6d249abbb4c94
aaajeong <qkrdkwjd59@naver.com>
2018-11-03 05:59:50 +0900

    setting더 바꿈

--- app/src/main/java/com/premom/www/premom3/SettingActivity.java
+++ app/src/main/java/com/premom/www/premom3/SettingActivity.java
@@ -1,29 +1,57 @@
 package com.premom.www.premom3;
 
+import android.app.Service;
 import android.content.Intent;
-import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class SettingActivity extends PreferenceActivity {
+public class SettingActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        addPreferencesFromResource(R.xml.setting_preference);
+    }
+
+    public void inform_btn(View v){
+        Intent inform_Intent;
+        inform_Intent = new Intent(this, InformActivity.class);
+        startActivity(inform_Intent);
+
+        finish();
+    }
+
+    public void service_btn(View v){
+        Intent service_Intent;
+        service_Intent = new Intent(this, ServiceHelpActivity.class);
+        startActivity(service_Intent);
+
+        finish();
+    }
+
+    public void logout_btn(View v){
+        Intent logout_Intent;
+        logout_Intent = new Intent(this, LogoutActivity.class);
+        startActivity(logout_Intent);
+
+        finish();
+    }
 
     }
 
-//    public void Btn_to_Main(View v){
-//        Intent intent;
-//        intent = new Intent(this, MainActivity.class);
-//        startActivity(intent);
-//
-//        finish();
-//
-//    }
-}
+
+
+
+
+
