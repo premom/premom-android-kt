145606f2427feedbfa0a5013e4706f97a1efb8ad
sinda72 <sinda72@gmail.com>
2018-11-10 00:23:46 +0900

    login

--- app/src/main/java/com/premom/www/premom3/LoginActivity.java
+++ app/src/main/java/com/premom/www/premom3/LoginActivity.java
@@ -8,15 +8,44 @@ import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.app.Activity;
+import android.app.ProgressDialog;
+
+import java.io.IOException;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.kt.gigaiot_sdk.GigaIotOAuth;
+import com.kt.gigaiot_sdk.PushApi;
+import com.kt.gigaiot_sdk.data.GiGaIotOAuthResponse;
+import com.kt.gigaiot_sdk.network.ApiConstants;
+import com.kt.iot.mobile.GiGaIotApplication;
+import com.kt.iot.mobile.android.R;
+import com.kt.iot.mobile.utils.ApplicationPreference;
+import com.kt.iot.mobile.utils.Blur;
+import com.kt.iot.mobile.utils.BitmapUtil;
+import com.kt.iot.mobile.utils.Common;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity implements View.OnClickListener {
+
     Button m_refButton;
     EditText user_id;
     EditText user_pw;
     private SharedPreferences appData;
     private boolean saveLoginData;
 
+    private GoogleCloudMessaging mGcm;
+
+    mGcm = GoogleCloudMessaging.getInstance(LoginActivity.this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +54,8 @@ public class LoginActivity extends AppCompatActivity {
         this.m_refButton =(Button)findViewById(R.id.fl_button01);
         this.user_id =(EditText)findViewById(R.id.user_id);
         this.user_pw =(EditText)findViewById(R.id.user_pw);
+        user_id.setTypeface(GiGaIotApplication.getDefaultTypeFace());
+        user_pw.setTypeface(GiGaIotApplication.getDefaultTypeFace());
 
         appData = getSharedPreferences("appData", MODE_PRIVATE);
         load();
@@ -35,14 +66,36 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    public void fl_BtnClick(View v){
-        if(user_id.getText().toString().equals("test") && user_pw.getText().toString().equals("test") ) {
-            login();
-        }  else {
-            Toast.makeText(this, "아이디 또는 패스워드가 잘못되었습니다.", Toast.LENGTH_SHORT).show();
-        }
-    }
+//    public void fl_BtnClick(View v){
+//        if(user_id.getText().toString().equals("test") && user_pw.getText().toString().equals("test") ) {
+//            login();
+//        }  else {
+//            Toast.makeText(this, "아이디 또는 패스워드가 잘못되었습니다.", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+public void onClick(View v) {
+    switch(v.getId()){
 
+        case R.id.iv_login_bt:
+
+            String id = mEtId.getText().toString();
+            String pw = mEtPw.getText().toString();
+
+            if(TextUtils.isEmpty(id)){
+                Toast.makeText(LoginActivity.this, R.string.login_id_empty, Toast.LENGTH_SHORT).show();
+                return;
+
+            }else if(TextUtils.isEmpty(pw)){
+                Toast.makeText(LoginActivity.this, R.string.login_pw_empty, Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            new LoginTask().execute();
+
+            break;
+
+    }
+}
     private void login() {
         save();
 
@@ -57,7 +110,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private void save() {
         SharedPreferences.Editor editor = appData.edit();
-        
+
         editor.putBoolean("SAVE_LOGIN_DATA", true);
         editor.putString("user_id", "test");
         editor.putString("user_pw", "test");
@@ -70,5 +123,8 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
-
-}
+    @Override
+//    public void onClick(View v) {
+//
+//    }
+//}
